89a3725a6dbe5bc99c1d491fde6e8da4384f25b8
gregorthebigmac <gregorthebigmac@gmail.com>
2019-04-26 10:15:49 -0500

    removed old script and replaced with auto-cat

--- install_bashrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# bashrc: My custom aliases and functions that I use all the time.
-# To append this to the end of your .bashrc file, execute this script:
-
-cat bashrc >> ~/.bashrc
-source ~/.bashrc
-
-# and you're all done! Enjoy!
