10869eafc15c2891fc0cc868746b5ea6e56205aa
gregorthebigmac <gregorthebigmac@gmail.com>
2019-06-12 15:25:03 -0500

    fixed variables

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ echo "what should I do when finished?"
 echo "[r]eboot [p]oweroff [n]othing"
 read answer
 
-if [[ "$1" == "r" ]]; then
+if [[ "$answer" == "r" ]]; then
 	apt-get update && apt upgrade -y && apt autoremove -y && reboot
-elif [[ "$1" == "p" ]]; then
+elif [[ "$answer" == "p" ]]; then
 	apt-get update && apt upgrade -y && apt autoremove -y && poweroff
-elif [[ "$1" == "n" ]]; then
+elif [[ "$answer" == "n" ]]; then
 	apt-get update && apt upgrade -y && apt autoremove -y
 else
 	echo "That wasn't an option, so I'll drop back out to the terminal when finished."
