c6ec488babad49fca20fe505a5415ea9792bccae
gregorthebigmac <gregorthebigmac@gmail.com>
2020-09-11 08:58:26 -0500

    added extensions to install OTB

---
+++ install_extensions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install -y gnome-tweaks \
+&& apt-get install -y gnome-shell-extension-appindicator \
+&& apt-get install -y gnome-shell-extension-caffeine \
+&& apt-get install -y gnome-shell-extension-dash-to-panel \
+&& apt-get install -y gnome-shell-extension-desktop-icons \
+&& apt-get install -y gnome-shell-extension-move-clock \
+&& apt-get install -y gnome-shell-extension-prefs \
+&& apt-get install -y gnome-shell-extension-show-ip \
+&& apt-get install -y gnome-shell-extension-system-monitor \
+&& apt-get install -y gnome-shell-extension-ubuntu-dock \
+&& apt-get install -y gnome-shell-extension-workspaces-to-dock \
+&& apt-get install -y gnome-shell-extensions
